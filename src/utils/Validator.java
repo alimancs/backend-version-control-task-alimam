4d5c5afed36dbf4ff1c1fb1214d478c1cba30b41
alimancs <aliman2952003@gmail.com>
2026-08-05 01:38:15 +0100

    feat: Added validation result printing method 2

--- src/utils/Validator.java
+++ src/utils/Validator.java
@@ -46,11 +46,5 @@ public class Validator {
         System.out.println("Valid: " + validateStudent(student));
     }
 
-    public static void printValidationResult(Student student) {
-        String result = validateWithMessage(student);
-        System.out.println("Validation result: " + result);
-        System.out.println("Valid: " + validateStudent(student));
-    }
-
 
 }
